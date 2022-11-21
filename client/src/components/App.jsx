cc340fa5438320b517d8ddbc0b192ce13a57f4ce
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-11-21 15:09:09 -0800

    useState to contain multiple cities and weather

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,14 +7,19 @@ function App() {
   //make api call using city name and render temp
   const [cityTemperature, setCityTemperature] = useState(0);
   const [cityName, setCityName] = useState("");
-  const [weatherContent, setWeatherContent] = useState([
-    {
-      name: "test",
-      temp: "",
-    },
-  ]);
-
-  console.log(weatherContent[0].name);
+  //object that holds single city and weather
+  const [cityWeather, setCityWeather] = useState({
+    name: "",
+    temp: "",
+  });
+  //an array of objects that contains mutliple cities and weather
+  const [citiesArr, setCitiesArr] = useState([{}]);
+  // const [weatherContent, setWeatherContent] = useState([
+  //   {
+  //     name: "test",
+  //     temp: "",
+  //   },
+  // ]);
 
   //why is react still rendering city temp when any other route is used
   useEffect(() => {
@@ -33,13 +38,33 @@ function App() {
         "imperial"
     )
       .then((response) => response.json())
-      .then((json) =>
-        setWeatherContent([
-          ...weatherContent,
-          { name: json.name, temp: json.main.temp },
-        ])
+      .then((data) =>
+        setCityWeather({ name: data.name, temp: data.main.temp })
       );
-  });
+  }, [cityName]);
+
+  // console.log(cityWeather);
+  console.log(citiesArr);
+
+  // useEffect(() => {
+  //   fetch(
+  //     "https://api.openweathermap.org/data/2.5/weather?q=" +
+  //       cityName +
+  //       "&appid=" +
+  //       "e0510037a8dda7f1b676abfb08e30234" +
+  //       "&units=" +
+  //       "imperial"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) =>
+  //       setWeatherContent((prevWeatherContent) => {
+  //         return [
+  //           ...prevWeatherContent,
+  //           { name: data.name, temp: data.main.temp },
+  //         ];
+  //       })
+  //     );
+  // }, [cityName]);
 
   //seperate weatherContent to be just an object
   //create a new states that is an array that can be used to store multiple weather objects
@@ -49,10 +74,13 @@ function App() {
   //json.main.temp
   //json.name
 
-  console.log([...weatherContent, { name: "London", temp: "69" }]);
+  // console.log([...weatherContent, { name: "London", temp: "69" }]);
 
   function addCityWeather(city) {
-    setCityName(city);
+    setCityName(city); //used as a dependecy for useEffect to fetch api request
+    setCitiesArr((prevCities) => {
+      return [...prevCities, cityWeather];
+    });
   }
 
   return (
